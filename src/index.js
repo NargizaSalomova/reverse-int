8393719893b2c47ec2631628f9aee4c49ad3919d
Nargiza Salomova <nargizusya@gmail.com>
2024-08-02 16:10:45 +0500

    feat: add task reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  reverse = 0
+
+  while (n!=0){
+    reverse = reverse * 10 + (n % 10);
+    n = Math.floor(n/10);
+  }
+  return reverse;
 }
+
+reverse(261);
