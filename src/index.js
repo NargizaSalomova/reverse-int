38041d3f1fb4c58da9cc4111f484dc8de45d1b4d
Nargiza Salomova <nargizusya@gmail.com>
2024-08-04 13:36:50 +0500

    feat: add changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 module.exports = function reverse (n) {
-  reverse = 0
+  let reverse = 0
 
-  while (n!=0){
+  while(n!=0){
     reverse = reverse * 10 + (n % 10);
     n = Math.floor(n/10);
   }
   return reverse;
 }
 
-reverse(261);
